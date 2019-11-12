f4e2907fdc288aa8738e400a6404b42c65ffb1e3
Diegod <diegoribfernandes@hotmail.com>
2019-11-12 00:34:39 -0200

    Models "Usuário e Venda" (Ajustados)

--- VarCont/src/main/java/br/com/fatec/VarCont/DataSource/Models/Usuario.java
+++ VarCont/src/main/java/br/com/fatec/VarCont/DataSource/Models/Usuario.java
@@ -2,68 +2,64 @@ package br.com.fatec.VarCont.DataSource.Models;
 
 import javax.persistence.*;
 @Entity
+@Table(name = "tbl_usuario")
 public class Usuario {
-//
-//@Id
-//long id;
-//
-//@Column(name = "usuario_nome")
-//String nome;
-//
-//@Column(name = "usuario_email")
-//String email;
-//
-//@Column(name = "usuario_senha")
-//String senha;
-//    
-//@Column(name = "usuario_administrador")
-//boolean admin;//
-//    public Usuario(int id, String nome, String Email, String Senha) {
-//        this.id = id;
-//        this.nome = nome;
-//        this.Email = Email;
-//        this.Senha = Senha;
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getNome() {
-//        return nome;
-//    }
-//
-//    public void setNome(String nome) {
-//        this.nome = nome;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getSenha() {
-//        return senha;
-//    }
-//
-//    public void setSenha(String senha) {
-//        this.senha = senha;
-//    }
-    
-//    public String getAdmin() {
-//        return admin;
-//    }
-//
-//    public void setAdmin(String admin) {
-//        this.admin = admin;
-//    }
 
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+@Column(name = "usuario_id")        
+long id;
+
+@Column(name = "usuario_nome")
+String nome;
+
+@Column(name = "usuario_email")
+String email;
+
+@Column(name = "usuario_senha")
+String senha;
+    
+@Column(name = "usuario_administrador")
+boolean admin;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
 
 }
--- VarCont/src/main/java/br/com/fatec/VarCont/DataSource/Models/Venda.java
+++ VarCont/src/main/java/br/com/fatec/VarCont/DataSource/Models/Venda.java
@@ -2,58 +2,58 @@ package br.com.fatec.VarCont.DataSource.Models;
 import java.util.Date;
 import javax.persistence.*;
 
-//@Entity
-//@Table(name = "tbl_venda")
+@Entity
+@Table(name = "tbl_venda")
 public class Venda {
     
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    @Column(name = "ven_id")
-//    long id;
-//
-//    @Column(name = "ven_data")
-//    private Date data;
-//    /*
-//    Precisamos fazer um jeito de fazer a data DD/MM/AA HH/MM
-//    */
-////    @Column (name = "IdUsuario")
-////    private long IdUsuario;
-////    
-////    @Column (name = "IdProduto")
-////    private long IdProduto;
-//    
-//    @Column (name = "Quantidade")
-//    private int quantidade;
-//
-//    public Venda(long id, int quantidade) {
-//        this.id = id;
-//        this.quantidade = quantidade;
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
-//    public Date getData() {
-//        return data;
-//    }
-//
-//    public void setData(Date data) {
-//        this.data = data;
-//    }
-//
-//    public int getQuantidade() {
-//        return quantidade;
-//    }
-//
-//    public void setQuantidade(int Quantidade) {
-//        this.quantidade = Quantidade;
-//    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ven_id")
+    long id;
+
+    @Column(name = "ven_data")
+    private Date data;
+    /*
+    Precisamos fazer um jeito de fazer a data DD/MM/AA HH/MM
+    */
+    @Column (name = "IdUsuario")
+    private long IdUsuario;
     
+    @Column (name = "IdProduto")
+    private long IdProduto;
+    
+    @Column (name = "Quantidade")
+    private int quantidade;
+
+    public Venda(long id, int quantidade) {
+        this.id = id;
+        this.quantidade = quantidade;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int Quantidade) {
+        this.quantidade = Quantidade;
+    }
+  
     
     
     
