e73141c632e80594dbf0b6cc326ff00f0432b633
Victor Martins <victormartinstinoco@live.com>
2019-11-20 15:46:16 -0300

    Começando a estruturar a funcionalidade de login (estática)

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -2,7 +2,7 @@
 function ApiLogin(){
     var api = new Object;
 
-    api.Login(user, successAction, errorAction) {
+    api.Login = function(user, successAction, errorAction) {
         // Temporary - Ajax or Axios code to access api
         let answer = (user.login == 'user@user.com' && user.password == 'user123') ? true : false;
         successAction(answer);
--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,8 +1,30 @@
+const inputEmail = $('#inpEmail');
+const inputPassword = $('#inpPassword') 
+const formSubmit = $('#formSubmit');
+
 $(function() {
     setBackgroundSize();
 })
 
 function setBackgroundSize() {
     windowSize = $(window).height();
-    $('.bem-vindo').height(windowSize);
+    $('.right-container').height(windowSize);
 }
+
+formSubmit.submit(function(form){
+    form.preventDefault();
+    const user = {
+        email: inputEmail.val(),
+        password: inputPassword.val()
+    };
+
+    const api = ApiLogin();
+    api.Login(user, function(aswer){
+        if (answer)
+            console.log('Login efetuado com sucesso.');
+        else
+            console.log('Email ou senha incorreto.');
+    }, function(erro){
+        console.log('Ocorreu um erro ao acessar a API de Login');
+    })
+})
