b6d61d05f6fa485bbf381d932290735acee2869e
Victor Martins <victormartinstinoco@live.com>
2019-11-23 02:06:34 -0300

    Ajuste em erro na listagem do usuário

--- frontend/js/usuarios.js
+++ frontend/js/usuarios.js
@@ -1,5 +1,6 @@
 $(function () {
     onChangeRadios();
+    expandCardEvent();
     listUsers();
 })
 
@@ -49,35 +50,36 @@ function expandCardEvent() {
 function listUsers(){
     api = ApiUsuario();
     api.Listar(function(response){
-        let text = '';
-        response.forEach(user => {
+        response.data.forEach(user => {
             user.admin = (user.admin) ? 'administrador' : 'caixa';
             let html = '<div class="card-item p-3 mb-3">' +
-                        '<div class="header-card cursor-pointer d-flex justify-content-between align-items-center">' +
-                            '<h2>' + user.nome + '</h2>'
-                            '<i class="material-icons expand">expand_more</i>'
+                '<div class="header-card cursor-pointer d-flex justify-content-between align-items-center">' +
+                    '<h2>' + user.nome + '</h2>' +
+                    '<i class="material-icons expand">expand_more</i>' +
+                '</div>' +
+                '<div class="body-card d-none">' +
+                    '<div class="d-flex justify-content-between align-items-end">' +
+                        '<div class="d-flex flex-column mt-3">' +
+                            '<h3>Email:</h3>' +
+                            '<div class="userInfo mb-2">' + user.email + '</div>'+
+                            '<h3>Senha:</h3>' +
+                            '<div class="userInfo mb-2">' + user.senha + '</div>' +
+                            '<h3>Tipo de Usuário</h3>' +
+                            '<div class="userInfo">' + user.admin + '</div>' +
                         '</div>' +
-                        '<div class="body-card d-none">' +
-                            '<div class="d-flex justify-content-between align-items-end">' +
-                                '<div class="d-flex flex-column mt-3">' +
-                                    '<h3>Email:</h3>' +
-                                    '<div class="userInfo mb-2">' + user.email + '</div>' +
-                                    '<h3>Senha:</h3>' +
-                                    '<div class="userInfo mb-2">' + user.senha + '</div>' +
-                                    '<h3>Tipo de Usuário</h3>' +
-                                    '<div class="userInfo">' + user.admin + '</div>' +
-                                '</div>'
-                                '<button class="btnEdit" data-id="' + user.id + '" data-toggle="modal" data-target="#edit-modal">Editar</button>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>'
+                        '<button class="btnEdit" data-id="' + user.id + '" data-toggle="modal" data-target="#edit-modal">Editar</button>' +
+                    '</div>' +
+                '</div>' +
+            '</div>';
+
+            text += html;
         })
-        $('#list-users').html(text);
+        $('.list-users').html(text);
         expandCardEvent();
     }, function(){}, function(){}, function(){
         const toast = {
             title: 'Erro na listagem de usuários',
-            message: 'Há um problema com a aplicação, entre em contato com o suporte.'  
+            message: 'Há um problema com a aplicação, entre em contato com o suporte.'    
         }
         NewToast(toast);
     })
